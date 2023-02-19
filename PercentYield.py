bc331a1801783be42819a6e8ad70d784e0b9d7f4
zharkzaurus <116230197+zharkzaurus@users.noreply.github.com>
2023-02-19 11:29:39 -0600

    Create PercentYield.py

---
+++ PercentYield.py
@@ -0,0 +1,6 @@
+import sys
+print("Percent Yield")
+actual=float(input("what is your actual yield\n"))
+theoretical=float(input("what is your theoretical yield\n"))
+answer =(actual/theoretical)*100
+print("your yield is ", answer)
