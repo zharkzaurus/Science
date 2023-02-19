2a3e37a6597343f100f10c60f6affc93be0e9933
zharkzaurus <116230197+zharkzaurus@users.noreply.github.com>
2023-02-19 11:32:58 -0600

    Create Stoichiometry.py

---
+++ Stoichiometry.py
@@ -0,0 +1,7 @@
+print("Stoichiometry")
+import sys
+MolesOfElement=float(input("Enter the mole of your element\n"))
+FirstMoleRatio=float(input("Enter the first mole ratio\n"))
+SecondMoleRatio=float(input("Enter the second mole ratio\n"))
+Result=MolesOfElement*FirstMoleRatio/SecondMoleRatio
+print('Your result is', Result)
